03b4a26d72c1f831f5cab76c07672e85620a721c
Michael Kohl <citizen428@forem.com>
2021-07-16 12:04:40 +0700

    Limit number of header fields and update card content

--- app/models/profile_field.rb
+++ app/models/profile_field.rb
@@ -1,6 +1,9 @@
 class ProfileField < ApplicationRecord
   include ActsAsProfileField
 
+  HEADER_FIELD_LIMIT = 3
+  HEADER_LIMIT_MESSAGE = "maximum number of header fields (#{HEADER_FIELD_LIMIT}) exceeded".freeze
+
   # Key names follow the Rails form helpers
   enum input_type: {
     text_field: 0,
@@ -20,10 +23,19 @@ class ProfileField < ApplicationRecord
   validates :display_area, presence: true
   validates :input_type, presence: true
   validates :show_in_onboarding, inclusion: { in: [true, false] }
+  validate :maximum_header_field_count
 
   def type
     return :boolean if check_box?
 
     :string
+  end
+
+  private
+
+  def maximum_header_field_count
+    return if self.class.header.count <= HEADER_FIELD_LIMIT
+
+    errors.add(:display_area, HEADER_LIMIT_MESSAGE)
   end
 end
